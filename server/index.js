4076c77677e43206df52746b819b9fa32a1c215e
Hubert Pixels <hubertpixels@Mikes-MacBook-Pro-2.local>
2026-06-22 15:53:42 +0800

    Fix Express 5 catch-all route syntax and add Node engine version

--- server/index.js
+++ server/index.js
@@ -251,7 +251,7 @@ app.get('/api/nearby-restaurants', async (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../dist')));
-  app.get('*', (req, res) => {
+  app.get('/{*path}', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
   });
 }
